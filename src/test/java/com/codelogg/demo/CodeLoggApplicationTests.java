d87ebeecd82be96b37df8b655e322f914e1a97d4
SMJminjeong <sdwp0920@gmail.com>
2024-03-11 18:35:59 +0900

    Initial commit

---
+++ src/test/java/com/codelogg/demo/CodeLoggApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codelogg.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeLoggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
