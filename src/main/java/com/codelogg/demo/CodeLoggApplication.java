2946e2ad0af0bf78fc4cb95585a00a56f123116a
SMJminjeong <sdwp0920@gmail.com>
2024-03-11 18:47:58 +0900

    :fire: remove lombok test class

--- src/main/java/com/codelogg/demo/CodeLoggApplication.java
+++ src/main/java/com/codelogg/demo/CodeLoggApplication.java
@@ -9,11 +9,6 @@ public class CodeLoggApplication {
 	//lombok 설정 후 preferences > annotation Processonrs > enabled annotation prpcessing 체크하기
 	public static void main(String[] args) {
 
-		Hello hello = new Hello();
-		hello.setData("hello");
-		String data = hello.getData();
-		System.out.println("data = " + data);
-
 		SpringApplication.run(CodeLoggApplication.class, args);
 	}
 
